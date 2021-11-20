05c0a9d55ed5c341623da7f3b39dbd972552a103
subelta <awaspecial10@gmail.com>
2021-11-20 19:14:08 +0300

    set command tooltips, nodemon

--- src/index.js
+++ src/index.js
@@ -3,24 +3,31 @@ import { TOKEN } from '../config.js';
 
 const bot = new TelegramBot(TOKEN, { polling: true });
 
+bot.setMyCommands([
+	{ command: 'set_question', description: 'Will set question' },
+	{ command: 'set_interval', description: 'Will set interval' },
+	{ command: 'stop', description: 'Will stop' },
+	{ command: 'restart', description: 'Will restart' },
+]);
+
 bot.on('message', (msg) => {
 	const { chat, text } = msg;
 
 	switch (text) {
 		case '/set_question': {
-			bot.sendMessage(chat.id, 'Will set question');
+			bot.sendMessage(chat.id, 'Yeah, will set question');
 			break;
 		}
 		case '/set_interval': {
-			bot.sendMessage(chat.id, 'Will set interval');
+			bot.sendMessage(chat.id, 'Yeah, will set interval');
 			break;
 		}
 		case '/stop': {
-			bot.sendMessage(chat.id, 'Will stop');
+			bot.sendMessage(chat.id, 'Yeah, will stop');
 			break;
 		}
 		case '/restart': {
-			bot.sendMessage(chat.id, 'Will restart');
+			bot.sendMessage(chat.id, 'Yeah, will restart');
 			break;
 		}
 		default: {
