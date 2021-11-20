efe2678e7966d6f0920febf4803bfd1c27fa3686
subelta <awaspecial10@gmail.com>
2021-11-20 18:44:54 +0300

    commands boilerplate

--- src/index.js
+++ src/index.js
@@ -3,15 +3,29 @@ import { TOKEN } from '../config.js';
 
 const bot = new TelegramBot(TOKEN, { polling: true });
 
-bot.onText(/\/echo (.+)/, (msg, match) => {
-	const chatId = msg.chat.id;
-	const resp = match[1];
-
-	bot.sendMessage(chatId, resp);
-});
-
 bot.on('message', (msg) => {
-	const chatId = msg.chat.id;
+	const { chat, text } = msg;
 
-	bot.sendMessage(chatId, 'Received your message');
+	switch (text) {
+		case '/set_question': {
+			bot.sendMessage(chat.id, 'Will set question');
+			break;
+		}
+		case '/set_interval': {
+			bot.sendMessage(chat.id, 'Will set interval');
+			break;
+		}
+		case '/stop': {
+			bot.sendMessage(chat.id, 'Will stop');
+			break;
+		}
+		case '/restart': {
+			bot.sendMessage(chat.id, 'Will restart');
+			break;
+		}
+		default: {
+			// console.log(msg);
+			bot.sendMessage(chat.id, 'I don\'t understand');
+		}
+	}
 });
